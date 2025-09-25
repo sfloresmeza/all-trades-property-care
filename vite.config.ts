826c8e12e857d07dbfe26575300805b34bcc30da
sfloresmeza <santiagofloresmeza@gmail.com>
2025-09-25 11:30:47 -0600

    Remove base for pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'all-trades-property-care',
     resolve: {
         alias: {
             '@': resolve(__dirname, './src'),
